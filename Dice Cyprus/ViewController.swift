06780faa9413def906859dbc9650461ce4c58251
taha murat <ttahamuratdoser@gmail.com>
2023-05-15 18:30:20 +0300

    last commit

--- Dice Cyprus/ViewController.swift
+++ Dice Cyprus/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollClicked(_ sender: Any) {
+        let diceArray = [UIImage(imageLiteralResourceName: "diceone"), UIImage(imageLiteralResourceName: "dicetwo"), UIImage(imageLiteralResourceName: "dicethree"), UIImage(imageLiteralResourceName: "dicefour"), UIImage(imageLiteralResourceName: "dicefive"), UIImage(imageLiteralResourceName: "dicesix") ]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
+    
 }
 
